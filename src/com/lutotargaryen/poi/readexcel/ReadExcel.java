5542536825d9ccee16c867921839555da1a0ba7b
luto <luto@yuntu-PC>
2017-04-10 14:00:13 +0800

    newInstance 方法需要的参数改为 connection对象

--- src/com/lutotargaryen/poi/readexcel/ReadExcel.java
+++ src/com/lutotargaryen/poi/readexcel/ReadExcel.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -69,51 +70,22 @@ public class ReadExcel implements Serializable{
 
 	// 使用volatile关键字保证多线程访问时readExcel的可见性
 	private volatile static ReadExcel readExcel;
+	
 	/**
 	 * 创建ReadExcel对象
-	 * 如果不设置UserName和Password则默认数据库连接用户为root,数据库连接密码也为root
-	 * @param Driver 数据库驱动类路径
-	 * @param DBUrl 数据库连接url
+	 * @param connection	数据库连接对象
 	 * @return
-	 * @throws RepeatCreateObject 对象重复创建异常
+	 * @throws RepeatCreateObject
 	 */
-	public static ReadExcel newInstance(String Driver,String DBUrl) throws RepeatCreateObject{
+	public static ReadExcel newInstance(Connection connection) throws RepeatCreateObject{
 		if(readExcel == null){
 			synchronized (ReadExcel.class) {
 				if(readExcel == null){
 					readExcel = new ReadExcel();
-					JdbcUtil.setDRIVERCLASS(Driver);
-					JdbcUtil.setURL(DBUrl);
-					JdbcUtil.setUSERNAME("root");
-					JdbcUtil.setPASSWROD("root");
+					JdbcUtil.setConnnection(connection);
 				}
 			}
 			
-		}else{
-			throw new RepeatCreateObject("This class can have only one instance object, and you have created an instance object");
-		}
-		return readExcel;
-	}
-	/**
-	 * 创建ReadExcel对象
-	 * @param Driver 数据库驱动类路径
-	 * @param DBUrl	数据库连接url
-	 * @param UserName	数据库连接用户名
-	 * @param PassWord	数据库连接密码
-	 * @return
-	 * @throws RepeatCreateObject 
-	 */
-	public static ReadExcel newInstance(String Driver,String DBUrl,String UserName,String PassWord) throws RepeatCreateObject{
-		if(readExcel == null){
-			synchronized (ReadExcel.class) {
-				if(readExcel == null){
-					readExcel = new ReadExcel();
-					JdbcUtil.setDRIVERCLASS(Driver);
-					JdbcUtil.setURL(DBUrl);
-					JdbcUtil.setUSERNAME(UserName);
-					JdbcUtil.setPASSWROD(PassWord);
-				}
-			}
 		}else{
 			throw new RepeatCreateObject("This class can have only one instance object, and you have created an instance object");
 		}
@@ -357,5 +329,5 @@ public class ReadExcel implements Serializable{
 		}
 		return excelFileds;
 	}
-
 }
+
--- src/com/lutotargaryen/poi/test/Test.java
+++ src/com/lutotargaryen/poi/test/Test.java
@@ -1,16 +1,31 @@
 package com.lutotargaryen.poi.test;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import com.lutotargaryen.poi.exception.RepeatCreateObject;
 import com.lutotargaryen.poi.readexcel.ReadExcel;
 
+
 public class Test {
-	public static void main(String[] args) throws RepeatCreateObject, IOException {
-		ReadExcel readExcel = ReadExcel.newInstance("com.mysql.jdbc.Driver","jdbc:mysql://127.0.0.1:3306/readexcel?userUnicode=true&characterEncoding=UTF-8");
-		
-		int s = readExcel.readExcelToMysql("D://ChoiceInfo.XLS");
-		System.out.println(s);
-	
+	public static void main(String[] args){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/readexcel?userUnicode=true&characterEncoding=UTF-8","root","root");
+			System.out.println("Test connection : " + connection);
+			ReadExcel readExcel = ReadExcel.newInstance(connection);
+			int i = readExcel.readExcelToMysql("D://ChoiceInfo.XLS");
+			System.out.println(i);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (RepeatCreateObject e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
